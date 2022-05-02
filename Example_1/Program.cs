9ea170e5514d1d38975bd28e4178e668e170ad35
Marina <troskom@mail.ru>
2022-05-02 18:54:04 +0300

    DZ_Seminar_5

---
+++ Example_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте массив, заполненный случайными положительными трехзначными числами. 
+// Написать программу, которая покажет количество четных числе в массиве.
+
+Console.WriteLine(" Введите количество элементов массива " );
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[arrayLength];
+FillArray(numbers);
+WriteArray(numbers);
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+}
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+int evenNumbers = 0;
+for (int i = 0; i <numbers.Length; i++)
+{
+    if(numbers[i] %2 == 0)
+    { 
+       evenNumbers = evenNumbers+1;
+    }
+}
+Console.WriteLine($" Количество четных элементов: " +evenNumbers);
